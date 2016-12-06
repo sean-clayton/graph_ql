11a492bea0c0393ca6fa0562c2759399fbd22964
Sean Clayton 🎷🐢 <sclayton@swifttrip.com>
2016-12-06 17:07:26 -0500

    Setup guardian

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,15 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :guardian, Guardian,
+  allowed_algos: ["HS512"], # optional
+  verify_module: Guardian.JWT, # optional
+  issuer: "GraphQl",
+  ttl: { 15, :minutes },
+  verify_issuer: true, # optional
+  secret_key: "mvKb03WjVdJ8Z1GEI2ISM/r7X2PFeCeDE0AAMrYyvyPYXmRUp2vrQfoBtJl7GYoU",
+  serializer: GraphQl.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
