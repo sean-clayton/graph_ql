03ab5e729810f905d66d07e4c9dda2a21a759701
Sean Clayton 🎷🐢 <sclayton@swifttrip.com>
2016-12-06 15:11:48 -0500

    Add seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,12 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     GraphQl.Repo.insert!(%GraphQl.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias GraphQl.{User,Post,Repo}
+
+Repo.insert!(%User{name: "Sean Clayton", email: "sean@seanclayton.me"})
+Repo.insert!(%User{name: "Bob Builder", email: "bob@canwefix.it"})
+
+for _ <- 1..10 do
+  Repo.insert!(%Post{
+    title: Faker.Lorem.sentence,
+    body: Faker.Lorem.paragraph,
+    user_id: [1, 2] |> Enum.take_random(1) |> hd
+  })
+end
